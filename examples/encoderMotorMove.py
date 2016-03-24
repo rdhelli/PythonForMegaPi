f0934a5a6b3a75d9d5c8710faf2a600c2f6425c1
indream <flashindream@gmail.com>
2016-03-24 11:14:51 +0800

    moved

---
+++ examples/encoderMotorMove.py
@@ -0,0 +1,19 @@
+from megapi import *
+
+def onForwardFinish(slot):
+	sleep(0.4);
+	bot.encoderMotorMove(slot,100,-1000,onBackwardFinish);
+
+def onBackwardFinish(slot):
+	sleep(0.4);
+	print slot;
+	bot.encoderMotorMove(slot,100,1000,onForwardFinish);
+
+if __name__ == '__main__':
+	bot = MegaPi()
+	bot.start()
+	bot.encoderMotorRun(3,0);
+	sleep(1);
+	onForwardFinish(3);
+	while 1:
+		continue;
---
+++ examples/stepperMoveTo.py
@@ -0,0 +1,18 @@
+from megapi import *
+
+def onForwardFinish():
+	sleep(0.4);
+	bot.stepperMoveTo(1,0,500,onBackwardFinish);
+
+def onBackwardFinish():
+	sleep(0.4);
+	bot.stepperMoveTo(1,1000,500,onForwardFinish);
+
+if __name__ == '__main__':
+	bot = MegaPi()
+	bot.start()
+	bot.stepperStop(1);
+	sleep(1);
+	onForwardFinish();
+	while 1:
+		continue;
